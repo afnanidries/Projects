4fcafdc1dcfaad3c6657e9cd5762ce64c6ff591a
ahidries <77736257+ahidries@users.noreply.github.com>
2025-05-27 20:12:37 -0400

    Add .gitignore and initial project files

---
+++ foundation-matcher/utils/twitter_feed.py
@@ -0,0 +1,7 @@
+def get_brand_updates():
+    # Mock Twitter updates for MVP
+    return [
+        {'brand': 'Fenty Beauty', 'tweet': 'Just dropped 10 new shades of our best-selling foundation!'},
+        {'brand': 'NARS', 'tweet': 'Now in mini sizes – perfect for travel season!'},
+        {'brand': 'Maybelline', 'tweet': 'Get 25% off all Fit Me products this weekend only!'}
+    ]
